39cd046f196ea745665c4a9cedc285a2fff475a7
Kumar Reddy <z993415@thryv.com>
2018-10-05 22:21:45 -0500

    Changes made to function from working directory.

--- src/test/java/com/objects/NewObject.java
+++ src/test/java/com/objects/NewObject.java
@@ -5,5 +5,9 @@ public class NewObject {
 	public void testingMethod() {
 		String a = "this is for testing";
 	}
+	
+	public void testingMethod(String a) {
+		a = "this is for method added from Working directory";
+	}
 
 }
